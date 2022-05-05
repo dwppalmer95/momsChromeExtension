c55c9c545151c66d7fef4a1376ef426e8556f29b
david palmer <dwppalmer95@gmail.com>
2022-05-05 13:38:02 -0400

    added photos, completed code to replace a background photo

--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   const imageContainer = document.getElementById("bg");
-  imageContainer.backgroundImage = "url('assets/1.jpg')";
+  imageContainer.style.backgroundImage = "url('assets/1.jpg')";
   // document.body.style.backgroundImage = "url('assets/1.jpg')";
 });
