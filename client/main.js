701aa9965099a23b429b452976fbde7415d507fc
david palmer <dwppalmer95@gmail.com>
2022-05-06 21:05:46 -0400

    added server

--- client/main.js
+++ client/main.js
@@ -1,5 +1,7 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  const imageContainer = document.getElementById("bg");
-  imageContainer.style.backgroundImage = "url('assets/3.jpg')";
-  // document.body.style.backgroundImage = "url('assets/1.jpg')";
+window.addEventListener('DOMContentLoaded', async (event) => {
+  fetch('http://localhost:3000/', { mode: 'no-cors'})
+    .then(res => console.log(res))
+    // .then(data => console.log(data))
+  // const imageContainer = document.getElementById("bg");
+  // imageContainer.style.backgroundImage = "url('assets/3.jpg')";
 });
---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  const fileNames = fs.readdirSync(path.join(__dirname, '../client/assets/nature'));
+  const randomInteger = Math.floor(Math.random() * fileNames.length);
+  const pattern = `^${randomInteger}_.*`;
+  const regex = new RegExp(pattern);
+  let imgPath;
+  for (let i = 0; i < fileNames.length; i++) {
+    const fileName = fileNames[i];
+    if (fileName.match(regex)) {
+      imgPath = path.resolve(fileName);
+      console.log(imgPath);
+      break;
+    }
+  }
+  res.set({
+    'Access-Control-Allow-Origin': 'chrome-extension://jegelamlcopnofagkldfnmpebeecdcch'
+  })
+  res.send({ imgPath });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
